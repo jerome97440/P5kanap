8fb1fe2d10f55f7ecf98a650475592d8deddf499
PADRE Jerome <juan974.pj16@gmail.com>
2022-10-31 17:41:27 +0400

    p5

--- front/js/product.js
+++ front/js/product.js
@@ -1,78 +1,18 @@
-let params = new URL(window.location.href).searchParams;
-let newID = params.get('id');
-let picture = document.querySelector(".item__img")
-let title = document.getElementById("title")
-let price = document.getElementById("price")
-let description = document.getElementById("description")
-let colorsArray = document.getElementById("colors")
-
-fetch("http://localhost:3000/api/products/"+ newID)
-.then((response) => response.json())
-.then((product) =>{
-        console.log(product)
-   
-        let image = document.createElement("img");
-        image.setAttribute('src', product.imageUrl);
-        image.setAttribute('alt', product.altTxt);
-        picture.appendChild(image);
-
-        title.textContent = product.name;
-        description.textContent = product.description;
-        price.textContent = product.price;
-       
-        for (let i=0; i < product.colors.length; i++) {
-            let color = document.createElement("option");
-            color.setAttribute('value', product.colors[i]);
-            color.textContent = product.colors[i];
-            colorsArray.appendChild(color)
-           
-        }
-    });
-  let imageURL = "";
-  let imageAlt = "";
-  const selectQuantity = document.getElementById("quantity");
-  const selectColors =document.getElementById("colors");
-  const addToCart = document.getElementById("addToCart");
+let idProduct = new URL(window.location.href).searchParams.get("id"); 
+console.log(idProduct);
   
-  addToCart.addEventListener('click',(event)=> {
-  event.preventDefault();
+let picture     = document.querySelector(".item__img"); 
+let title       = document.getElementById("title"); 
+let price       = document.getElementById("price"); 
+let description = document.getElementById("description"); 
+let colorsArray = document.getElementById("colors"); 
 
-    const selection = {
-        id: newID,
-        image: imageURL,
-        alt: imageAlt,
-        name: title.textContent,
-        price: price.textContent,
-        color: selectColors.value,
-        quantity: selectQuantity.value,
-      }
-      let productInLocalStorage =  JSON.parse(localStorage.getItem('product'));
-      const addProductLocalStorage = () => {
-        localStorage.setItem('product', JSON.stringify(productInLocalStorage));
-  }
-  let addConfirm = () => {
-    alert('Le produit a bien été ajouté au panier');
-  }
-
-  let update = false;
-  
-  if (productInLocalStorage) {
-   productInLocalStorage.forEach (function (productOk, key) {
-    if (productOk.id == newID && productOk.color == selectColors.value) {
-      productInLocalStorage[key].quantity = parseInt(productOk.quantity) + parseInt(selectQuantity.value);
-      localStorage.setItem('product', JSON.stringify(productInLocalStorage));
-      update = true;
-      addConfirm();
-    }
-  });
-    if (!update) {
-    addProductLocalStorage();
-    addConfirm();
-    }
-  }
-  else {
-    productInLocalStorage = [];
-    addProductLocalStorage();
-    addConfirm();
-  }
-});
+const apelfectch = function (){
+  fetch("http://localhost:3000/api/products/" + idProduct)
+.then (function(res){
+    return res.json();
+})
+.then (function(data) {
+    console.log(data)
+})
+}
--- front/js/script.js
+++ front/js/script.js
@@ -1,33 +1,48 @@
-fetch("http://localhost:3000/api/products")
-.then((response) => response.json())
-.then(function a (products){
 
-    console.log(products)
-    products.forEach(product => {
-        console.log(product)
-        
-      //Crée les 5 éléments 
-      //faire un getElementById pour ciblé la section et ajouter les 5 élements crée
+function getProducts(){ 
+return fetch("http://localhost:3000/api/products")
+.then (function(res){
+    return res.json();
+})
+.then (function(data) {
+  console.log(data)
+  })
+  .catch (function(error){
+    console.log(error)
+  })
+}
 
+function displayProduct (products) {
+
+
+   
+      for (let i in products) {
       let items = document.getElementById("items");
       let link = document.createElement("a");
-      link.setAttribute('href',`./product.html?id=${product._id}`);
+      link.href = `./product.html?id=${i._id}`;
       items.appendChild(link);
 
       let article = document.createElement("article");
       link.appendChild(article);
 
       let images = document.createElement("img");
-      images.setAttribute('src', product.imageUrl)
-      images.setAttribute('alt', product.altTxt);
+      images.setAttribute('src', i.imageUrl)
+      images.setAttribute('alt', i.altTxt);
       article.appendChild(images);
 
       let title = document.createElement("h3");
-      title.textContent = product.name;
+      title.textContent = i.name;
       article.appendChild(title);
 
       let description = document.createElement("p");
       article.appendChild(description);
-      description.textContent = product.description;
-    });
-});
+      description.textContent =i.description;
+ 
+      }
+    }
+  async function main (){
+    const products = await getProducts ();
+    displayProduct (products)
+
+  }
+  main()
