e58151bfd7d3972cee47989ffee260703ad3ca21
PADRE Jerome <juan974.pj16@gmail.com>
2022-11-02 14:04:12 +0400

    p5indexproductok

--- front/js/script.js
+++ front/js/script.js
@@ -1,48 +1,40 @@
 
-function getProducts(){ 
-return fetch("http://localhost:3000/api/products")
-.then (function(res){
-    return res.json();
-})
-.then (function(data) {
-  console.log(data)
-  })
-  .catch (function(error){
-    console.log(error)
-  })
+function getProducts() {
+  return fetch("http://localhost:3000/api/products")
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 
-function displayProduct (products) {
+function displayProduct(products) {
+  for (let i in products) {
+    const product = products[i];
+    let items = document.getElementById("items");
+    let link = document.createElement("a");
+    link.href = `./product.html?id=${product._id}`;
+    items.appendChild(link);
 
+    let article = document.createElement("article");
+    link.appendChild(article);
 
-   
-      for (let i in products) {
-      let items = document.getElementById("items");
-      let link = document.createElement("a");
-      link.href = `./product.html?id=${i._id}`;
-      items.appendChild(link);
+    let images = document.createElement("img");
+    images.setAttribute("src", product.imageUrl);
+    images.setAttribute("alt", product.altTxt);
+    article.appendChild(images);
 
-      let article = document.createElement("article");
-      link.appendChild(article);
+    let title = document.createElement("h3");
+    title.textContent = product.name;
+    article.appendChild(title);
 
-      let images = document.createElement("img");
-      images.setAttribute('src', i.imageUrl)
-      images.setAttribute('alt', i.altTxt);
-      article.appendChild(images);
-
-      let title = document.createElement("h3");
-      title.textContent = i.name;
-      article.appendChild(title);
-
-      let description = document.createElement("p");
-      article.appendChild(description);
-      description.textContent =i.description;
- 
-      }
-    }
-  async function main (){
-    const products = await getProducts ();
-    displayProduct (products)
-
+    let description = document.createElement("p");
+    description.textContent = product.description;
+    article.appendChild(description);
   }
-  main()
+}
+async function main() {
+  const products = await getProducts();
+  displayProduct(products);
+}
+main();
